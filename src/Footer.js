ce7cf8efe494385f7e0da46197b15c67e94e66f8
Vidhu <vidhu.v9@gmail.com>
2021-05-28 15:54:17 +0530

    layout

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <div>
+            I am a footer 
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+    return (
+        <div>
+            I am header
+        </div>
+    )
+}
+
+export default Header
